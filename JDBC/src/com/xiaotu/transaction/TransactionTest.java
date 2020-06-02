c63aa61dd3c110c761e694fec2d3e42ffa399cc2
zhangxiao <zhangxiao@163.com>
2020-06-02 18:17:11 +0800

    事物隔离级别

--- JDBC/src/com/xiaotu/transaction/TransactionTest.java
+++ JDBC/src/com/xiaotu/transaction/TransactionTest.java
@@ -3,9 +3,8 @@ package com.xiaotu.transaction;
 import com.xiaotu.util.JDBCUtils;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.lang.reflect.Field;
+import java.sql.*;
 
 /**
  *
@@ -124,6 +123,84 @@ public class TransactionTest {
             JDBCUtils.closeResource(null, ps);
         }
         return 0;
+    }
+
+
+    //******************************************************************
+    @Test
+    public void testTransactionSelect() throws Exception{
+
+        Connection conn = JDBCUtils.getConnection();
+        //获取当前连接的隔离级别
+        System.out.println(conn.getTransactionIsolation());
+        //设置数据库的隔离级别：
+        conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+        //取消自动提交数据
+        conn.setAutoCommit(false);
+
+        String sql = "select user,password,balance from user_table where user = ?";
+        User user = getInstance(conn, User.class, sql, "CC");
+
+        System.out.println(user);
+
+    }
+
+    @Test
+    public void testTransactionUpdate() throws Exception{
+        Connection conn = JDBCUtils.getConnection();
+
+        //取消自动提交数据
+        conn.setAutoCommit(false);
+        String sql = "update user_table set balance = ? where user = ?";
+        update(conn, sql, 5000,"CC");
+
+        Thread.sleep(15000);
+        System.out.println("修改结束");
+    }
+
+    //通用的查询操作，用于返回数据表中的一条记录（version 2.0：考虑上事务）
+    public <T> T getInstance(Connection conn,Class<T> clazz,String sql, Object... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            // 获取结果集的元数据 :ResultSetMetaData
+            ResultSetMetaData rsmd = rs.getMetaData();
+            // 通过ResultSetMetaData获取结果集中的列数
+            int columnCount = rsmd.getColumnCount();
+
+            if (rs.next()) {
+                T t = clazz.newInstance();
+                // 处理结果集一行数据中的每一个列
+                for (int i = 0; i < columnCount; i++) {
+                    // 获取列值
+                    Object columValue = rs.getObject(i + 1);
+
+                    // 获取每个列的列名
+                    // String columnName = rsmd.getColumnName(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    // 给t对象指定的columnName属性，赋值为columValue：通过反射
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columValue);
+                }
+                return t;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+
+        }
+
+        return null;
     }
 
 }
